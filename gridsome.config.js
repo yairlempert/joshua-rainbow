2b3d7007f40cb6b0b4da12efa0feaafcf56c2748
Joshua O'Sullivan <jrosullivan1@sheffield.ac.uk>
2020-03-27 23:09:33 +0000

    Implement sitemap

--- gridsome.config.js
+++ gridsome.config.js
@@ -38,7 +38,36 @@ module.exports = {
       options: {
         id: 'UA-146872812-1'
       }
-    },    
+    },
+    {
+      use: '@gridsome/plugin-sitemap',
+      options: {
+        cacheTime: 600000, // default
+        exclude: [],
+        config: {
+          '/': {
+            changefreq: 'monthly',
+            priority: 1
+          },
+          '/contact': {
+            changefreq: 'monthly',
+            priority: 0.9
+          },
+          '/photography/**': {
+            changefreq: 'monthly',
+            priority: 0.8
+          },
+          '/darkroom/**': {
+            changefreq: 'monthly',
+            priority: 0.7
+          },
+          '/programming': {
+            changefreq: 'monthly',
+            priority: 0.6
+          }
+        }
+      }
+    }    
   ],
   transformers: {
     remark: {
